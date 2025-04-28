859a76dd1be1344e617b34219fd8329aa4488a8f
adrian galan pacheco <agalanpa@uoc.edu>
2025-04-28 11:33:32 +0200

    Exercise 7 w/o standalone

--- PEC4_Ej_Prac/ecommerce/src/app/article-item/article-item.component.ts
+++ PEC4_Ej_Prac/ecommerce/src/app/article-item/article-item.component.ts
@@ -1,39 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+/* Exercise 7: Import Input, Output, EventEmitter */
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Article } from '../models/article';
-import { NgClass, NgIf } from '@angular/common';
+/* Exercise 7: Import ArticleQuantityChange */
+import { ArticleQuantityChange } from '../models/article-quantity-change';
 
 @Component({
   selector: 'app-article-item',
   templateUrl: './article-item.component.html',
   styleUrl: './article-item.component.css',
 })
-export class ArticleItemComponent implements OnInit {
+export class ArticleItemComponent {
   // Defines an article
-  public article!: Article;
+  /* Exercise 7: Recieves an Article as an Input */
+  @Input() article!: Article;
 
-  constructor() {}
+  /* Exercise 7: Emits an event when the quantity changes */
+  @Output() quantityChange = new EventEmitter<ArticleQuantityChange>();
 
-  // Initializes the article with default values
-  ngOnInit() {
-    this.article = {
-      name: 'Camiseta',
-      imageUrl:
-        'https://img.freepik.com/psd-premium/t-shirt-negro-filmado-estudio-sobre-fondo-blanco_1153121-10726.jpg',
-      price: 9.99,
-      isOnSale: true,
-      quantityInCart: 0,
-    };
-  }
-
-  // Increments quantity
-  incrementQuantity() {
-    this.article.quantityInCart++;
-  }
-
-  // Decrements quantity
-  decrementQuantity() {
-    if (this.article.quantityInCart > 0) {
-      this.article.quantityInCart--;
-    }
+  changeQuantity(quantity: number) {
+    this.quantityChange.emit({
+      article: this.article,
+      quantityChange: quantity,
+    });
   }
 }
