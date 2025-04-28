6a0a0bca4e77b6328d4871b0e5a6ca9c04641d64
adrian galan pacheco <agalanpa@uoc.edu>
2025-04-28 11:03:52 +0200

    Exercise 3 w/o standalone

---
+++ PEC4_Ej_Prac/ecommerce/src/app/article-item/article-item.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Article } from '../models/article';
+
+@Component({
+  selector: 'app-article-item',
+  templateUrl: './article-item.component.html',
+  styleUrl: './article-item.component.css',
+})
+export class ArticleItemComponent implements OnInit {
+  // Defines an article
+  public article!: Article;
+
+  constructor() {}
+
+  // Initializes the article with default values
+  ngOnInit() {
+    this.article = {
+      name: 'Camiseta',
+      imageUrl:
+        'https://img.freepik.com/psd-premium/t-shirt-negro-filmado-estudio-sobre-fondo-blanco_1153121-10726.jpg',
+      price: 9.99,
+      isOnSale: true,
+      quantityInCart: 0,
+    };
+  }
+
+  // Increments quantity
+  incrementQuantity() {
+    this.article.quantityInCart++;
+  }
+
+  // Decrements quantity
+  decrementQuantity() {
+    if (this.article.quantityInCart > 0) {
+      this.article.quantityInCart--;
+    }
+  }
+}
